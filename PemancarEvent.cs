ce443ecb51d95ad46cf87a00bdeac07e2ca803d1
fatahhillah365-pixel <fatahhillah365@gmail.com>
2026-09-07 11:02:58 +0700

    PemancarEvent.cs

---
+++ PemancarEvent.cs
@@ -0,0 +1,20 @@
+#PTS_GameDev_11PPLG3
+
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PemancarEvent : MonoBehaviour
+{
+    public static event Action SaatTombolDitekan;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Debug.Log("Tombol ditekan");
+            SaatTombolDitekan?.Invoke();
+        }
+    }
+}
