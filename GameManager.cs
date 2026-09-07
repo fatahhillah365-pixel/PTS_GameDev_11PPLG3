e13a85ccb7e140aba27cd112ea5782ea00ab6697
fatahhillah365-pixel <fatahhillah365@gmail.com>
2026-09-07 10:55:01 +0700

    GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,39 @@
+#PTS_GameDev_11PPLG3
+
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    public int totalKoin;
+
+    private int koinTerkumpul = 0;
+
+
+    void Start()
+    {
+        // Menghitung jumlah Coin yang ada di scene
+        totalKoin = GameObject.FindGameObjectsWithTag("Coin").Length;
+
+        Debug.Log("Total Coin : " + totalKoin);
+    }
+
+
+    public void AmbilKoin()
+    {
+        // Menambah jumlah koin yang diambil
+        koinTerkumpul++;
+
+        // Jika semua koin sudah diambil
+        if (koinTerkumpul == totalKoin)
+        {
+            Menang();
+        }
+    }
+
+
+    void Menang()
+    {
+        Debug.Log("KAMU MENANG!");
+    }
+}
