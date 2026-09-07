a29539eca8a840886274b69bb14d9de91f097685
fatahhillah365-pixel <fatahhillah365@gmail.com>
2026-09-07 10:59:23 +0700

    stateZombie.cs

---
+++ stateZombie.cs
@@ -0,0 +1,11 @@
+# PTS_GameDev_11PPLG3
+
+using UnityEngine;
+
+public enum StateZombie
+{
+    IDLE,                   //diam menungu
+    PATROL,                 //berkeliling area
+    CHASE,                 //mengejar player
+    ATTACK                //menyerang player
+}
